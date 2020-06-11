fe01eedfd17b1affb30e8f0fb9f8243f72f9036d
Razbir <roybutterfly019@gmail.com>
2020-06-12 03:54:52 +0600

    cart commit"

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import duplicateData from '../../duplicateData';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import {addToDatabaseCart, getDatabaseCart} from '../../utilities/databaseManager'
 
 const ItemDetails = () => {
-    const {productName} = useParams()
-    const seeData = duplicateData.find(id => id.name === productName)
-    console.log(seeData)
+    const {productName} = useParams();
+    const [store, setStore] = useState([]);
+    const [cart, setCart] = useState([]);
+    const addHandle = () =>{
+        const seeData = duplicateData.find(id => id.name === productName);
+        setStore(seeData)
+    }
+    
+    const addHandle2 = (props) =>{
+        const cartProduct = [...cart, props];
+        setCart(cartProduct);
+        const sameItem = cartProduct.filter(id => id.name === props.name);
+        const count = sameItem.length;
+        addToDatabaseCart(props.name, count);
+    }
+
+    const deletHandel = () => {
+        const savedCart = getDatabaseCart();
+        const itemName = Object.keys(savedCart)
+        console.log(itemName)
+    }
+
+   useEffect(()=>{
+       addHandle();
+   }, [])
+    const {name} = store;
+    
     return (
         <div>
-           <h1>This is my product</h1> 
+            <div>
+              <h1>{name}</h1>
+            </div>
+            <div>
+                <button onClick={()=> addHandle2(store)}>+</button>
+                <button onClick={()=> deletHandel()}>-</button>
+                <h2>{cart.length}</h2>
+            </div>
         </div>
+        
     );
 };
 
